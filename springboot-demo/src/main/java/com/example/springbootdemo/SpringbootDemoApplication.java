449b5b2b55dce05eb2f03bf679f1ca39afcd5268
wenlinshan <1396523950@qq.com>
2021-09-17 16:29:11 +0800

    @Async的应用

---
+++ springboot-demo/src/main/java/com/example/springbootdemo/SpringbootDemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+public class SpringbootDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDemoApplication.class, args);
+    }
+
+}
---
+++ springboot-demo/src/test/java/com/example/springbootdemo/SpringbootDemoApplicationTests.java
@@ -0,0 +1,27 @@
+package com.example.springbootdemo;
+
+import com.example.springbootdemo.task.AsyncTask;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.CompletableFuture;
+
+@SpringBootTest
+class SpringbootDemoApplicationTests {
+    @Autowired
+    private AsyncTask asyncTasks;
+
+    @Test
+    public void test() throws Exception {
+        long start = System.currentTimeMillis();
+        CompletableFuture<String> one = asyncTasks.doTaskOne();
+        CompletableFuture<String> two = asyncTasks.doTaskTwo();
+        CompletableFuture<String> three = asyncTasks.doTaskThree();
+        //CompletableFuture.allOf(one, two, three).join();
+        System.out.println(one.get() + ";" + two.get() + ";" + three.get());
+        long end = System.currentTimeMillis();
+        System.out.println("总耗时：" + (end - start) + "毫秒");
+    }
+
+}
