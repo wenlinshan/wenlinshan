9e4b061de9a53c3fa7f57011399371db158ffebb
wenlinshan <1396523950@qq.com>
2022-05-07 17:30:13 +0800

    init

---
+++ vipwen-dynamicDataSource/vipwen-dynamicDataSource/src/main/java/cn/vipwen/vipwendynamicdatasource/config/DynamicDataSource.java
@@ -0,0 +1,12 @@
+package cn.vipwen.vipwendynamicdatasource.config;
+
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+
+public class DynamicDataSource extends AbstractRoutingDataSource {
+
+    @Override
+    protected Object determineCurrentLookupKey() {
+        return DataSourceContextHolder.get();
+    }
+
+}
